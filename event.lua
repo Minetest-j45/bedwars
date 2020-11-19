0144322aa9732eb676161f66486483ea101e1063
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2020-11-19 17:10:50 +0000

    Update event.lua

--- event.lua
+++ event.lua
@@ -1,25 +1,24 @@
-bedraid.events = {3000}
-bedraid.event_list = {"game_end"}
-bedraid.event_msg = {
+jewelraid.events = {3000}
+jewelraid.event_list = {"game_end"}
+jewelraid.event_msg = {
 	"Game has ended",
 }
 
-bedraid.event = 0
-
-bedraid.event_timer_start = function()
-	bedraid.timer = 0
+jewelraid.event = 0
+jewelraid.event_timer_start = function()
+	jewelraid.timer = 0
 	minetest.register_globalstep(function(dtime)
-		bedraid.timer = bedraid.timer + dtime
-		if bedraid.timer >= bedraid.events[bedraid.event + 1] then
-			bedraid.event = bedraid.event + 1
-			minetest.chat_send_all(bedraid.event_msg[bedraid.event])
-			bedraid.timer = 0
-			if bedraid.event_list[bedraid.event] == "game_end" then
+		jewelraid.timer = jewelraid.timer + dtime
+		if jewelraid.timer >= jewelraid.events[jewelraid.event + 1] then
+			jewelraid.event = jewelraid.event + 1
+			minetest.chat_send_all(jewelraid.event_msg[jewelraid.event])
+			jewelraid.timer = 0
+			if jewelraid.event_list[jewelraid.event] == "game_end" then
 				minetest.request_shutdown("Game has ended", false, 1)
 			end
 		end
-		if bedraid.event_list[bedraid.event] ~= "game_end" then
-			bedraid.ui_update()
+		if jewelraid.event_list[jewelraid.event] ~= "game_end" then
+			jewelraid.ui_update()
 		end
 	end)
 end
