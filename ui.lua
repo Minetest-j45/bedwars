4c5d9a974d947ad2e1d117486aecb88742a8684e
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2020-11-19 16:47:35 +0000

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -16,18 +16,14 @@ minetest.register_on_joinplayer(function(player)
 end)
 
 bedwars.ui_update = function()
-	local redbed, greenbed, bluebed, yellowbed
+	local redbed, bluebed
 	if bedwars.beds.red then redbed = "+" end
-	if bedwars.beds.green then greenbed = "+" end
 	if bedwars.beds.blue then bluebed = "+" end
-	if bedwars.beds.yellow then yellowbed = "+" end
 	
 	local text = "Current map: " .. bedwars.current_map .. "\n" ..
 	bedwars.next_event_msg[bedwars.event + 1] .. ": " .. tostring(math.floor(bedwars.events[bedwars.event + 1] - (bedwars.timer or 0))) .. "\n\n" ..
 	"R: " .. (redbed or #bedwars.teams.red) .. "\n" ..
-	"G: " .. (greenbed or #bedwars.teams.green) .. "\n" ..
-	"B: " .. (bluebed or #bedwars.teams.blue) .. "\n" ..
-	"Y: " .. (yellowbed or #bedwars.teams.yellow)
+	"B: " .. (bluebed or #bedwars.teams.blue)
 	
 	local players = minetest.get_connected_players()
 	for _, player in ipairs(players) do
