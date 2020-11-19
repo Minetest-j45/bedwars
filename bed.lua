21178f37dd31749b37091f60103d21d955702f85
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2020-11-19 17:05:37 +0000

    Update bed.lua

--- bed.lua
+++ bed.lua
@@ -1,6 +1,6 @@
-bedwars.beds = {red = true, blue = true}
+jewelraid.beds = {red = true, blue = true}
 
-bedwars.str_to_colour = function(str)
+jewelraid.str_to_colour = function(str)
 	local codes = {red = "#FF0000", blue = "#0000FF"}
 	return codes[str]
 end
@@ -16,35 +16,35 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
 				t.inv:remove_item(t.k, itemstack)
 			end, {k = k, inv = inv})
 		end
-		if bedwars.get_team_by_pos(pos) == bedwars.get_player_team(digger:get_player_name()) then
+		if jewelraid.get_team_by_pos(pos) == jewelraid.get_player_team(digger:get_player_name()) then
 			minetest.chat_send_player(digger:get_player_name(), "You can't destroy your own bed")
 			return
 		end
-		if not bedwars.beds[bedwars.get_team_by_pos(pos)] then return end
-		bedwars.beds[bedwars.get_team_by_pos(pos)] = false
-		minetest.chat_send_all("Team " .. minetest.colorize(bedwars.str_to_colour(bedwars.get_team_by_pos(pos)), bedwars.get_team_by_pos(pos)) .. "'s bed has been destroyed by " .. digger:get_player_name())
+		if not jewelraid.beds[jewelraid.get_team_by_pos(pos)] then return end
+		jewelraid.beds[jewelraid.get_team_by_pos(pos)] = false
+		minetest.chat_send_all("Team " .. minetest.colorize(jewelraid.str_to_colour(jewelraid.get_team_by_pos(pos)), jewelraid.get_team_by_pos(pos)) .. "'s bed has been destroyed by " .. digger:get_player_name())
 		minetest.sound_play("bed_destruction", {
 			pos = pos,
 			max_hear_distance = 100,
 			gain = 2.0,
 		})
-		bedwars.ui_update()
+		jewelraid.ui_update()
 	end
 end)
 
 minetest.register_on_dieplayer(function(player)
-	minetest.chat_send_all(minetest.colorize(bedwars.str_to_colour(bedwars.get_player_team(player:get_player_name())), player:get_player_name()) .. " died")
+	minetest.chat_send_all(minetest.colorize(jewelraid.str_to_colour(jewelraid.get_player_team(player:get_player_name())), player:get_player_name()) .. " died")
 end)
 
 minetest.register_on_respawnplayer(function(player)
 	local itemstack = ItemStack("default:sword_stone")
 	player:set_wielded_item(itemstack)
-	if not bedwars.beds[bedwars.get_player_team(player:get_player_name())] then
+	if not jewelraid.beds[jewelraid.get_player_team(player:get_player_name())] then
 		minetest.kick_player(player:get_player_name(), "You cannot respawn because your bed has been destroyed. Please wait for a new game to start.")
 		minetest.after(1, function()
 			local empty_teams = 0
 			local alive = {red = true, blue = true}
-			for k, v in pairs(bedwars.teams) do
+			for k, v in pairs(jewelraid.teams) do
 				if #v == 0 then
 					empty_teams = empty_teams + 1
 					alive[k] = false
@@ -57,13 +57,13 @@ minetest.register_on_respawnplayer(function(player)
 				end
 			end
 			if empty_teams == 1 then
-				minetest.chat_send_all("Team " .. minetest.colorize(bedwars.str_to_colour(last_team), last_team) .. " has won!")
+				minetest.chat_send_all("Team " .. minetest.colorize(jewelraid.str_to_colour(last_team), last_team) .. " has won!")
 				minetest.request_shutdown("Game has ended", false, 10) 
 			end
 		end, nil)
 	else
 		minetest.after(0, function(player)
-			player:set_pos(minetest.string_to_pos(bedwars.get_map_by_name(bedwars.current_map)[bedwars.get_player_team(player:get_player_name())]))
+			player:set_pos(minetest.string_to_pos(jewelraid.get_map_by_name(jewelraid.current_map)[jewelraid.get_player_team(player:get_player_name())]))
 		end, player)
 	end
 	return true
@@ -71,7 +71,7 @@ end)
 
 minetest.register_on_joinplayer(function(player)
 	minetest.after(1, function()
-		if not bedwars.beds[bedwars.get_player_team(player:get_player_name())] then
+		if not jewelraid.beds[jewelraid.get_player_team(player:get_player_name())] then
 			minetest.kick_player(player:get_player_name(), "You cannot respawn because your bed has been destroyed. Please wait for a new game to start.")
 		end
 	end)
