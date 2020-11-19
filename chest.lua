d00a7dd5779b246d399699e8b76ce9192efb0299
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2020-11-19 17:08:37 +0000

    Update chest.lua

--- chest.lua
+++ chest.lua
@@ -1,12 +1,12 @@
-bedwars.echest_fs = "size[10,7]" ..
+jewelraid.echest_fs = "size[10,7]" ..
 "list[current_player;ec;1,1;8,3;]" ..
 "list[current_player;main;1,5;8,1;]"
 
-bedwars.chest_fs = "size[10,7]" ..
+jewelraid.chest_fs = "size[10,7]" ..
 "list[context;tc;1,1;8,3;]" ..
 "list[current_player;main;1,5;8,1;]"
 
-minetest.register_node("bedwars:echest", {
+minetest.register_node("jewelraid:echest", {
 	description = "Ender chest",
 	tiles = {"echest_top.png", "echest_bottom.png", "echest_right.png", "echest_left.png", "echest_back.png", "echest_front.png"},
 	paramtype = "light",
@@ -21,11 +21,11 @@ minetest.register_node("bedwars:echest", {
 	},
 	groups = {choppy = 3},
 	on_construct = function(pos)
-		minetest.get_meta(pos):set_string("formspec", bedwars.echest_fs)
+		minetest.get_meta(pos):set_string("formspec", jewelraid.echest_fs)
 	end,
 })
 
-minetest.register_node("bedwars:chest", {
+minetest.register_node("jewelraid:chest", {
 	description = "Team chest",
 	tiles = {"chest_top.png", "chest_bottom.png", "chest_right.png", "chest_left.png", "chest_back.png", "chest_front.png"},
 	paramtype = "light",
@@ -40,19 +40,19 @@ minetest.register_node("bedwars:chest", {
 	},
 	groups = {choppy = 3},
 	on_construct = function(pos)
-		minetest.get_meta(pos):set_string("formspec", bedwars.chest_fs)
+		minetest.get_meta(pos):set_string("formspec", jewelraid.chest_fs)
 		minetest.get_inventory({type = "node", pos = pos}):set_size("tc", 24)
 	end,
 	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
-		if bedwars.get_team_by_pos(pos) == bedwars.get_player_team(player:get_player_name()) or (not bedwars.beds[bedwars.get_team_by_pos(pos)] and #bedwars.teams[bedwars.get_team_by_pos(pos)] == 0) then return count end
+		if jewelraid.get_team_by_pos(pos) == jewelraid.get_player_team(player:get_player_name()) or (not jewelraid.beds[jewelraid.get_team_by_pos(pos)] and #jewelraid.teams[jewelraid.get_team_by_pos(pos)] == 0) then return count end
 		return 0
 	end,
 	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-		if bedwars.get_team_by_pos(pos) == bedwars.get_player_team(player:get_player_name()) or (not bedwars.beds[bedwars.get_team_by_pos(pos)] and #bedwars.teams[bedwars.get_team_by_pos(pos)] == 0) then return stack:get_count() end
+		if jewelraid.get_team_by_pos(pos) == jewelraid.get_player_team(player:get_player_name()) or (not jewelraid.beds[jewelraid.get_team_by_pos(pos)] and #jewelraid.teams[jewelraid.get_team_by_pos(pos)] == 0) then return stack:get_count() end
 		return 0
 	end,
 	allow_metadata_inventory_take = function(pos, listname, index, stack, player)
-		if bedwars.get_team_by_pos(pos) == bedwars.get_player_team(player:get_player_name()) or (not bedwars.beds[bedwars.get_team_by_pos(pos)] and #bedwars.teams[bedwars.get_team_by_pos(pos)] == 0) then return stack:get_count() end
+		if jewelraid.get_team_by_pos(pos) == jewelraid.get_player_team(player:get_player_name()) or (not jewelraid.beds[jewelraid.get_team_by_pos(pos)] and #jewelraid.teams[jewelraid.get_team_by_pos(pos)] == 0) then return stack:get_count() end
 		return 0
 	end,
 })
