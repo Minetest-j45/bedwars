553b1f7d5d1826c40403e5af82d4cc142131377d
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-10 17:07:02 +0000

    Update map.lua

--- map.lua
+++ map.lua
@@ -115,7 +115,7 @@ end)
 local timer = 0
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime
-	if timer >= 2 then
+	if timer >= 1 then
 		local players = minetest.get_connected_players()
 		for _, player in ipairs(players) do
 			if player:get_pos().y < 80 then
