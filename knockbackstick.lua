f9b8e080af983d93ecd0a330dd6dd6d7ea9435cd
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-10 16:53:58 +0000

    add more knockback sticks

--- knockbackstick.lua
+++ knockbackstick.lua
@@ -1,10 +1,24 @@
-minetest.register_craftitem("jewelraid:kbstick", {
+minetest.register_craftitem("jewelraid:kbstick1", {
+	description = "Knockback stick",
+	inventory_image = "default_stick.png",
+})
+minetest.register_craftitem("jewelraid:kbstick2", {
+	description = "Knockback stick",
+	inventory_image = "default_stick.png",
+})
+minetest.register_craftitem("jewelraid:kbstick3", {
 	description = "Knockback stick",
 	inventory_image = "default_stick.png",
 })
 
 minetest.calculate_knockback = function(player, hitter, time_from_last_punch, tool_capabilities, dir, distance, damage)
-	if hitter:get_wielded_item():get_name() == "jewelraid:stick" then
+	if hitter:get_wielded_item():get_name() == "jewelraid:kbstick1" then
+		return 10
+	end
+	if hitter:get_wielded_item():get_name() == "jewelraid:kbstick2" then
+		return 15
+	end
+	if hitter:get_wielded_item():get_name() == "jewelraid:kbstick3" then
 		return 20
 	end
 	return 0
