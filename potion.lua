cd630ed4f01c6cd4fa2159da3db2185ef2a7c29d
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-12 16:35:17 +0000

    make it so you can have many potions at once and the effect accumulates

--- potion.lua
+++ potion.lua
@@ -3,49 +3,59 @@ minetest.register_craftitem("jewelraid:speed", {
 	inventory_image = "potion_speed.png",
 	stack_max = 1,
 	on_use = function(itemstack, user, pointed_thing)
+		local speed_modifier = user:get_physics_override().speed
+		local new_speed_modifier = speed_modifier * 1.5
 		user:set_physics_override({
-			speed = 1.5,
+			speed = new_speed_modifier,
 		})
 		itemstack:take_item()
 		minetest.after(30, function(user)
+			local latest_speed_modifier = user:get_physics_override().speed
+			local reset_speed_modifier = latest_speed_modifier/1.5
 			user:set_physics_override({
-				speed = 1,
+				speed = reset_speed_modifier,
 			})
 		end, user)
 		return itemstack
 	end,
 })
-
 minetest.register_craftitem("jewelraid:jump", {
 	description = "Jump potion",
 	inventory_image = "potion_jump.png",
 	stack_max = 1,
 	on_use = function(itemstack, user, pointed_thing)
+		local jump_modifier = user:get_physics_override().jump
+		local new_jump_modifier = jump_modifier * 2
 		user:set_physics_override({
-			jump = 2,
+			jump = new_jump_modifier,
 		})
 		itemstack:take_item()
 		minetest.after(30, function(user)
+			local latest_jump_modifier = user:get_physics_override().jump
+			local reset_jump_modifier = latest_jump_modifier/2
 			user:set_physics_override({
-				jump = 1,
+				jump = reset_jump_modifier,
 			})
 		end, user)
 		return itemstack
 	end,
 })
-
 minetest.register_craftitem("jewelraid:antigravity", {
 	description = "Anti-gravity potion",
 	inventory_image = "potion_antigravity.png",
 	stack_max = 1,
 	on_use = function(itemstack, user, pointed_thing)
+		local antigravity_modifier = user:get_physics_override().gravity
+		local new_antigravity_modifier = antigravity_modifier/2
 		user:set_physics_override({
-			gravity = 0.5,
+			gravity = new_antigravity_modifier,
 		})
 		itemstack:take_item()
 		minetest.after(30, function(user)
+			local latest_antigravity_modifier = user:get_physics_override().gravity
+			local reset_antigravity_modifier = latest_antigravity_modifier * 2
 			user:set_physics_override({
-				gravity = 1,
+				gravity = reset_antigravity_modifier,
 			})
 		end, user)
 		return itemstack
